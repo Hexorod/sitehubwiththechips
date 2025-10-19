2cbc058c8fe1f6c5740ea3f3ec976585d33268a9
Hexorod <145511133+Hexorod@users.noreply.github.com>
2025-10-18 23:52:58 -0400

    Delete panic.js

--- panic.js
+++
@@ -1,6 +0,0 @@
-// panic.js — Global key listener
-document.addEventListener("keydown", function(e) {
-  if (e.key === "`") {  // The backtick key
-    window.location.replace("https://classroom.google.com");
-  }
-});
